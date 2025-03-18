7bb541fd0f2eaab3969388897196c51f94218e58
Shivam yeshu <kumar04shivamy@gmail.com>
2025-03-19 02:10:34 +0530

    some minor changes

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Load and Read Excel File
-const workbook = xlsx.readFile("/dataSet/students.xlsx");
+const workbook = xlsx.readFile("./dataSet/students.xlsx");
 const sheet_name = workbook.SheetNames[0];
 const studentData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name]);
 
 app.get("/", (req,res) => {
-    res.send("home page route");
+    res.send("Welcome to Student API");
 })
 
 // Search API - Filter by Enrollment, Name, Branch, CGPA, Skills, etc.
@@ -29,19 +29,32 @@ app.get("/search", (req, res) => {
             (!cgpa || parseFloat(student["CGPA"]) >= parseFloat(cgpa)) &&
             (!placed || student["Placement Status"].toLowerCase() === placed.toLowerCase()) &&
             (!skills || student["Skills"].toLowerCase().includes(skills.toLowerCase())) &&
-            (!attendance || parseFloat(student["Attendance (%)"]) >= parseFloat(attendance))
+            (!attendance || parseFloat(student["Attendance (%)"]) >= parseFloat(attendance))&&
+            (!cgpa || parseFloat(student["CGPA"]) >= parseFloat(cgpa))
         );
     });
     
     res.json(results.length ? results : { message: "No student found" });
-    console.log(results[0].CGPA);
+    console.log(results[0].Name);
     
 });
 
+
+app.get('/sum', (req,res)=>{
+    const a = Number(req.query.a) //1
+    const b = Number(req.query.b) //2 
+
+   const sum =  a + b
+
+    
+    res.send(`The sum of ${a} and ${b} is ${sum}`)
+})
+
 app.use((req, res) => {
-    res.status(404).json({ message: "Route not available by shivam" });
+    res.status(404).json({ message: "Route not created by shivam" });
 });
+
 
 // Start Server
 const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
